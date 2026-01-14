10c4de1eb560123d0f8e9338313f08bd13929fe2
2heunxun <seapeon@naver.com>
2026-01-14 19:14:54 +0900

    [setting] First init Project

---
+++ src/main/java/com/deadlock/hellocs/HellocsApplication.java
@@ -0,0 +1,13 @@
+package com.deadlock.hellocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellocsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deadlock/hellocs/HellocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deadlock.hellocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
