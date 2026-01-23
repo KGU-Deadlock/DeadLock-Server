2bd0fdb965e1471fe9481c23555c1f8aeafcf0a4
L33gn21 <l33gn21@gmail.com>
2026-01-23 22:09:45 +0900

    FEAT: User Repository, builder 추가(#10)

--- src/main/java/com/deadlock/hellocs/domain/user/entity/User.java
+++ src/main/java/com/deadlock/hellocs/domain/user/entity/User.java
@@ -2,13 +2,19 @@ package com.deadlock.hellocs.domain.user.entity;
 
 import com.deadlock.hellocs.domain.quiz.domain.QuizLevel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "users")
 public class User {
 
---
+++ src/main/java/com/deadlock/hellocs/domain/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.deadlock.hellocs.domain.user.repository;
+
+import com.deadlock.hellocs.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findByNickname(String nickname);
+}
