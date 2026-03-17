8ecc271e407ecfd14be10fd6a7fe2cdf3828a08f
L33gn21 <l33gn21@gmail.com>
2026-03-17 13:14:35 +0900

    Refactor: getAdminAccount() 메소드 dev 폴더로 이동

--- src/main/java/com/deadlock/hellocs/user/adapter/in/web/UserController.java
+++ src/main/java/com/deadlock/hellocs/user/adapter/in/web/UserController.java
@@ -1,18 +1,15 @@
 package com.deadlock.hellocs.user.adapter.in.web;
 
-import com.deadlock.hellocs.global.jwt.JwtTokenProvider;
 import com.deadlock.hellocs.user.adapter.in.web.docs.UserControllerDocs;
 import com.deadlock.hellocs.user.application.port.in.CreateUserUseCase;
-import com.deadlock.hellocs.user.application.port.in.LoadUserUseCase;
 import com.deadlock.hellocs.user.application.port.in.ManageUserUseCase;
+import com.deadlock.hellocs.user.application.port.in.LoadUserUseCase;
 import com.deadlock.hellocs.user.application.port.in.dto.ProfileResult;
 import com.deadlock.hellocs.user.application.port.in.dto.UpdateMyInfoCommand;
 import com.deadlock.hellocs.user.application.port.in.dto.UserSignUpCommand;
 import com.deadlock.hellocs.global.apiPayload.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
@@ -25,7 +22,6 @@ public class UserController implements UserControllerDocs {
     private final CreateUserUseCase createUserUseCase;
     private final LoadUserUseCase loadUserUseCase;
     private final ManageUserUseCase manageUserUseCase;
-    private final JwtTokenProvider jwtTokenProvider;
 
     @PostMapping
     public ApiResponse<Void> createUser(@RequestBody @Valid UserSignUpCommand command,
@@ -56,17 +52,4 @@ public class UserController implements UserControllerDocs {
         return ApiResponse.onSuccess(null);
     }
 
-    @GetMapping("/admin")
-    public ApiResponse<AdminAccountTokenResponse> getAdminAccount() {
-        Long kakaoId = 1L;
-        Authentication authentication = new UsernamePasswordAuthenticationToken(kakaoId.toString(), null);
-
-        String accessToken = jwtTokenProvider.createAccessToken(authentication, "ADMIN");
-        String refreshToken = jwtTokenProvider.createRefreshToken(authentication);
-        boolean isUser = loadUserUseCase.isExist(kakaoId);
-
-        return ApiResponse.onSuccess(new AdminAccountTokenResponse(accessToken, refreshToken, isUser));
-    }
-
-    private record AdminAccountTokenResponse(String accessToken, String refreshToken, boolean isUser) {}
 }
