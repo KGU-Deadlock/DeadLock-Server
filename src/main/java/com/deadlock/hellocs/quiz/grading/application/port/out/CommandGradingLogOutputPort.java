6904e1ff7ef71bd59aee3e3de2bc088d9743a255
L33gn21 <l33gn21@gmail.com>
2026-04-24 00:45:29 +0900

    Refactor: 구 퀴즈 모듈 삭제(#80)

--- src/main/java/com/deadlock/hellocs/quiz/grading/application/port/out/CommandGradingLogOutputPort.java
+++
@@ -1,7 +0,0 @@
-package com.deadlock.hellocs.quiz.grading.application.port.out;
-
-import com.deadlock.hellocs.quiz.grading.domain.GradingLog;
-
-public interface CommandGradingLogOutputPort {
-    GradingLog save(GradingLog gradingLog);
-}
--- src/main/java/com/deadlock/hellocs/quiz/stt/application/port/in/CommandSttStreamInputPort.java
+++
@@ -1,8 +0,0 @@
-package com.deadlock.hellocs.quiz.stt.application.port.in;
-
-import com.deadlock.hellocs.quiz.stt.application.port.in.dto.SttChunkCommand;
-import com.deadlock.hellocs.quiz.stt.application.port.in.dto.SttChunkResult;
-
-public interface CommandSttStreamInputPort {
-    SttChunkResult process(SttChunkCommand command);
-}
