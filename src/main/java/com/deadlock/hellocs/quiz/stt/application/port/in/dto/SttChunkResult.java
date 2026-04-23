03bd084821345bcc3673faa199cf0a44aaf6d9a8
L33gn21 <l33gn21@gmail.com>
2026-04-24 00:45:29 +0900

    Refactor: 구 퀴즈 모듈 삭제(#80)

--- src/main/java/com/deadlock/hellocs/quiz/stt/application/port/in/dto/SttChunkResult.java
+++
@@ -1,9 +0,0 @@
-package com.deadlock.hellocs.quiz.stt.application.port.in.dto;
-
-public record SttChunkResult(
-        String sessionId,
-        Long sequence,
-        String text,
-        boolean isFinalChunk
-) {
-}
