2011699f82b2d80ce9e51d3e3da473eafd6277d6
2heunxun <seapeon@naver.com>
2026-05-29 12:47:51 +0900

    bugfix : interview submit miss error fix

--- interview/src/main/java/com/deadlock/hellocs/interview/session/adapter/in/web/InterviewController.java
+++ interview/src/main/java/com/deadlock/hellocs/interview/session/adapter/in/web/InterviewController.java
@@ -38,7 +38,7 @@ public class InterviewController implements InterviewControllerDocs {
     @PostMapping("/{interviewId}/answer")
     @Override
     public ApiResponse<Void> submitAnswer(
-            @PathVariable String interviewId,
+            @PathVariable("interviewId") String interviewId,
             @RequestBody @Valid SubmitAnswerRequest request
     ) {
         SubmitAnswerCommand command = new SubmitAnswerCommand(
@@ -53,7 +53,7 @@ public class InterviewController implements InterviewControllerDocs {
 
     @PostMapping("/{interviewId}/complete")
     @Override
-    public ApiResponse<FeedbackResult> completeInterview(@PathVariable String interviewId) {
+    public ApiResponse<FeedbackResult> completeInterview(@PathVariable("interviewId") String interviewId) {
         return ApiResponse.onSuccess(interviewCommandService.completeInterview(interviewId));
     }
 }
